578af865fb46f89fbf1047e32afc02894eda7114
nndd91 <nndd91@hotmail.com>
2017-05-30 17:02:17 +0800

    Add homepage, add create button

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,6 +3,23 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
+
+  def new
+    @tweet = Tweet.new
+  end
   
+  def create
+    @tweet = Tweet.new(tweet_params)
+  end
+
+  def destroy
+    @tweet.destroy
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:body, :user_id)
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'tweets' , to: 'tweets#index'
+  get 'tweets/new', to: 'tweets#new'
 end
