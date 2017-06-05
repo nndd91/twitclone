22e8c515410e5a226024168352eab196f209d1bf
ngerald51 <starhubber81@gmail.com>
2017-06-05 15:55:49 +0800

    Made changes to message controller indentation and model indendations

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,7 +3,7 @@ class Message < ApplicationRecord
   belongs_to :to_user_id, class_name: "User"
 
   def self.check_exist?(user, to_user_id)
-    Following.exists?(from_user_id: user, to_user_id: to_user_id)
+    Message.exists?(from_user_id: user, to_user_id: to_user_id)
   end
 
 end
