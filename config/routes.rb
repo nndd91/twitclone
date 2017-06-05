ee0d09309a1bcb6694f6495eeef5dc81860ea6d4
nndd91 <nndd91@hotmail.com>
2017-06-05 13:31:10 +0800

    Add new fields to sign up and update account page

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 Rails.application.routes.draw do
-	devise_for :users
+	devise_for :users, controllers: { registrations: 'users/registrations'}
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   root 'tweets#index'
 
   resources :tweets do
     collection do # collection instead of member so url = tweets/feed instead of tweets/:id/feed
-      get :feed 
+      get :feed
     end
   end
-  
+
   resources :profile, only: [:show] do
     member do
       get :get_following
       get :get_followers
     end
-    resources :followings, only: [:create, :destroy], shallow: true 
+    resources :followings, only: [:create, :destroy], shallow: true
     # shallow: true to reduce url length, also profile_id is redundant in delete.
   end
 
