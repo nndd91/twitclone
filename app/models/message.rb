9201d5373ec9a7460fa0623714eaab143efdce34
ngerald51 <starhubber81@gmail.com>
2017-06-05 17:26:56 +0800

    Added RSPEC test for message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,9 @@
 class Message < ApplicationRecord
-  belongs_to :from_user_id, class_name: "User"
-  belongs_to :to_user_id, class_name: "User"
+  belongs_to :from_user, class_name: "User"
+  belongs_to :to_user, class_name: "User"
 
+  validates :from_user_id, presence: true
+  validates :to_user_id, presence: true
   def self.check_exist?(user, to_user_id)
     Message.exists?(from_user_id: user, to_user_id: to_user_id)
   end
