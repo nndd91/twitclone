1fb41495e054370d8d0073330c713694b5d38711
nndd91 <nndd91@hotmail.com>
2017-06-06 21:32:54 +0800

    Add model spec

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,8 +2,8 @@ class Like < ApplicationRecord
 	belongs_to :user, class_name: "User"
 	belongs_to :tweet, class_name: "Tweet"
 
-  def self.if_liked?(tweet_id, user_id)
-    Like.find_by(user_id: user_id, tweet_id: tweet_id)
-  end
+  validates :tweet_id, uniqueness: { scope: :user_id,
+    message: "User can only like tweet once!" }, presence: true
+  validates :user_id, presence: true
 
 end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
+  it { should belong_to(:user).class_name("User") }
+  it { should belong_to(:tweet).class_name("Tweet") }
 
+  it { should validate_presence_of(:user_id) }
+
+  it { should validate_uniqueness_of(:tweet_id).scoped_to(:user_id).
+    with_message("User can only like tweet once!") }
+
+  it { should validate_presence_of(:tweet_id) }
 end
