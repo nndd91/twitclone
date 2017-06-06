4651c91b6ca7c61b283a62f49cb1cdda35c92247
nndd91 <nndd91@hotmail.com>
2017-06-06 11:24:20 +0800

    Config rails admin to authenticate

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -24,7 +24,7 @@ RailsAdmin.config do |config|
   # config.show_gravatar = true
 
   config.authorize_with do |controller|
-    unless current_user.try(:admin?)
+    unless current_user.try(:is_admin?)
       flash[:error] = "You are not admin"
       redirect_to main_app.root_path
     end
