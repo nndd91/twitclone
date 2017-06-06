45b054e90c0fe0d8dd276afbd462eb4b836f00a9
nndd91 <nndd91@hotmail.com>
2017-06-06 16:42:28 +0800

    Add Ajax

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,14 +2,26 @@ class LikesController < ApplicationController
 
   def create
     @tweet = Tweet.find(params[:tweet_id])
-    @tweet.likes.build(user_id: current_user.id)
-    redirect_to tweet_path(@tweet)
+    @like = @tweet.likes.build(user_id: current_user.id)
+    respond_to do |format|
+      if @like.save
+        format.html { redirect_to tweets_path }
+        format.js
+      end
+    end
   end
 
   def destroy
-    byebug
-    Like.find(params[:id]).destroy
-    redirect_to tweet_path(params[:tweet])
+    @like = Like.find(params[:id])
+    @tweetid = @like.tweet_id
+    @tweet = Tweet.find(@tweetid)
+    @like.destroy
+    respond_to do |format|
+      format.html {
+        redirect_to tweets_path
+      }
+      format.js
+    end
   end
 
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,4 +2,8 @@ class Like < ApplicationRecord
 	belongs_to :user, class_name: "User"
 	belongs_to :tweet, class_name: "Tweet"
 
+  def self.if_liked?(tweet_id, user_id)
+    Like.find_by(user_id: user_id, tweet_id: tweet_id)
+  end
+
 end
