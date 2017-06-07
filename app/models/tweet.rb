bcfdfb9631ba249396e947027d441bc1d7af38b1
nndd91 <nndd91@hotmail.com>
2017-06-07 13:09:00 +0800

    Add model for images

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,6 +2,8 @@ class Tweet < ApplicationRecord
   belongs_to :user
   validates :body, length: { maximum: 140 }, presence: true
   has_many :likes, dependent: :destroy
+  has_many :images, dependent: :destroy
+
   def self.get_tweets(following_ids, current_user_id)
     Tweet.where("user_id IN (?) OR user_id = ?", following_ids, current_user_id)
   end
