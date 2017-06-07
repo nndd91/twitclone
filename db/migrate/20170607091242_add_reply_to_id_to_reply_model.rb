c9519728e555eb4024c6eab2a17dddd2f781444e
ngerald51 <starhubber81@gmail.com>
2017-06-07 17:44:58 +0800

    Add column reply_to_id to Reply Model

--- db/migrate/20170607091242_add_reply_to_id_to_reply_model.rb
+++ db/migrate/20170607091242_add_reply_to_id_to_reply_model.rb
@@ -1,4 +1,5 @@
 class AddReplyToIdToReplyModel < ActiveRecord::Migration[5.1]
   def change
+     add_column :reply_to_id, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170607091242) do
+ActiveRecord::Schema.define(version: 20170607094304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20170607091242) do
     t.integer "tweet_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "reply_to_id"
   end
 
   create_table "tweets", force: :cascade do |t|
