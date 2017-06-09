fed7ff25920292392710e5dc7381472c05a6e382
nndd91 <nndd91@hotmail.com>
2017-06-09 15:34:08 +0800

    Generate Mention controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'mentions/create'
+
+  get 'mentions/destroy'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 	devise_for :users, controllers: { registrations: 'users/registrations'}
 
