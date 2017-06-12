c06c3fc6fa36a65e7177710b2e9145b8d7f28d68
nndd91 <nndd91@hotmail.com>
2017-06-12 15:18:38 +0800

    Add new column viewed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170612050424) do
+ActiveRecord::Schema.define(version: 20170612071751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20170612050424) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "viewed", default: false
     t.index ["tweet_id"], name: "index_mentions_on_tweet_id"
     t.index ["user_id"], name: "index_mentions_on_user_id"
   end
