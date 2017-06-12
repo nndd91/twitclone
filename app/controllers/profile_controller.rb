74cc702915b147c392af1c8f97bb54d687dbdf5d
nndd91 <nndd91@hotmail.com>
2017-06-12 13:29:27 +0800

    Add private check

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,19 +1,20 @@
 class ProfileController < ApplicationController
 
+  before_action :get_user, only: [:show, :get_following, :get_followers]
+  before_action :check_private, only: [:show]
+
   def show
-    @user = User.find(params[:id])
     @tweets = Tweet.where(user_id: @user.id)
     @following = find_following
   end
 
   def get_following
-    @user = User.find(params[:id])
     @relationship = @user.following
     @following = find_following
   end
 
   def get_followers
-    @user = User.find(params[:id])
+
     @relationship = @user.followers
     @following = find_following
   end
@@ -24,6 +25,19 @@ class ProfileController < ApplicationController
   def find_following
     Following.find_by(follower_id: current_user.id,
                       followed_id: @user.id)
+  end
+
+  def get_user
+    @user = User.find(params[:id])
+  end
+
+  def check_private
+    if @user.private
+      if !@user.followers.exists?(current_user.id)
+        flash[:notice] = "This profile is private!"
+        redirect_to root_path
+      end
+    end
   end
 
 end
